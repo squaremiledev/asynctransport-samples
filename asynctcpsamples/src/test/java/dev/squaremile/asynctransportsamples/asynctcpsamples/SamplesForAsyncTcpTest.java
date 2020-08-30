121a88dd420c56fa256140e11dcdf4cac4a171dd
Michael Szymczak <contact@michaelszymczak.com>
2020-08-31 00:32:48 +0100

    use the app launcher

--- asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
+++ asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
@@ -2,18 +2,18 @@ package dev.squaremile.asynctransportsamples.asynctcpsamples;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+import dev.squaremile.asynctcp.application.TransportAppLauncher;
+import dev.squaremile.asynctcp.domain.api.Transport;
 import dev.squaremile.asynctcp.domain.api.commands.Connect;
 import dev.squaremile.asynctcp.domain.api.commands.Listen;
 import dev.squaremile.asynctcp.domain.api.events.Connected;
 import dev.squaremile.asynctcp.domain.api.events.StartedListening;
-import dev.squaremile.asynctcp.nonblockingimpl.NonBlockingTransport;
 import dev.squaremile.asynctcp.testfitures.TransportEventsSpy;
 
 import static dev.squaremile.asynctcp.testfitures.FreePort.freePort;
@@ -23,27 +23,39 @@ class SamplesForAsyncTcpTest
 
     private TransportEventsSpy serverEvents;
     private TransportEventsSpy clientEvents;
-    private NonBlockingTransport serverSideTransport;
-    private NonBlockingTransport clientSideTransport;
+    private Transport serverTransport;
+    private Transport clientTransport;
 
     @BeforeEach
     void setUp() throws IOException
     {
         serverEvents = new TransportEventsSpy();
         clientEvents = new TransportEventsSpy();
-        serverSideTransport = new NonBlockingTransport(new TransportEventsRedirect(serverEvents));
-        clientSideTransport = new NonBlockingTransport(new TransportEventsRedirect(clientEvents));
+        final TransportEventsRedirect serverEventsRedirect = new TransportEventsRedirect(serverEvents);
+        final TransportEventsRedirect clientEventsRedirect = new TransportEventsRedirect(clientEvents);
+        new TransportAppLauncher().launch(
+                transport ->
+                {
+                    serverTransport = transport;
+                    return serverEventsRedirect::onEvent;
+                });
+        new TransportAppLauncher().launch(
+                transport ->
+                {
+                    clientTransport = transport;
+                    return clientEventsRedirect::onEvent;
+                });
     }
 
     @Test
     void establishConnection()
     {
         final int serverPort = freePort();
-        serverSideTransport.handle(serverSideTransport.command(Listen.class).set(1, serverPort));
+        serverTransport.handle(serverTransport.command(Listen.class).set(1, serverPort));
         assertThat(serverEvents.lastResponse(StartedListening.class, 1).port()).isEqualTo(serverPort);
 
         // When
-        clientSideTransport.handle(clientSideTransport.command(Connect.class).set(serverPort, 2));
+        clientTransport.handle(clientTransport.command(Connect.class).set("localhost", serverPort, 2, 100));
 
         // Then
         Connected connected = clientEvents.lastResponse(Connected.class, 2);
@@ -52,12 +64,5 @@ class SamplesForAsyncTcpTest
         assertThat(connected.connectionId()).isGreaterThan(0);
         assertThat(connected.inboundPduLimit()).isGreaterThan(0);
         assertThat(connected.outboundPduLimit()).isGreaterThan(0);
-    }
-
-    @AfterEach
-    void tearDown()
-    {
-        clientSideTransport.close();
-        serverSideTransport.close();
     }
 }
