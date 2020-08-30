767c1b2c5a2ab3a26d8a2caca5e1254c7d09cdb2
Michael Szymczak <contact@michaelszymczak.com>
2020-08-31 00:51:13 +0100

    better setup

--- asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
+++ asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+import dev.squaremile.asynctcp.application.Application;
 import dev.squaremile.asynctcp.application.TransportAppLauncher;
 import dev.squaremile.asynctcp.domain.api.Transport;
 import dev.squaremile.asynctcp.domain.api.commands.Connect;
 import dev.squaremile.asynctcp.domain.api.commands.Listen;
 import dev.squaremile.asynctcp.domain.api.events.Connected;
+import dev.squaremile.asynctcp.domain.api.events.Event;
 import dev.squaremile.asynctcp.domain.api.events.StartedListening;
 import dev.squaremile.asynctcp.testfitures.TransportEventsSpy;
 
@@ -20,30 +22,25 @@ import static dev.squaremile.asynctcp.testfitures.FreePort.freePort;
 
 class SamplesForAsyncTcpTest
 {
-
-    private TransportEventsSpy serverEvents;
-    private TransportEventsSpy clientEvents;
-    private Transport serverTransport;
-    private Transport clientTransport;
+    private WhiteBoxApplication serverApp;
+    private WhiteBoxApplication clientApp;
 
     @BeforeEach
     void setUp() throws IOException
     {
-        serverEvents = new TransportEventsSpy();
-        clientEvents = new TransportEventsSpy();
-        final TransportEventsRedirect serverEventsRedirect = new TransportEventsRedirect(serverEvents);
-        final TransportEventsRedirect clientEventsRedirect = new TransportEventsRedirect(clientEvents);
         new TransportAppLauncher().launch(
                 transport ->
                 {
-                    serverTransport = transport;
-                    return serverEventsRedirect::onEvent;
+                    // Store the reference to manipulate the underlying transport directly from the test.
+                    // A standard app does not require such things.
+                    serverApp = new WhiteBoxApplication(transport);
+                    return serverApp;
                 });
         new TransportAppLauncher().launch(
                 transport ->
                 {
-                    clientTransport = transport;
-                    return clientEventsRedirect::onEvent;
+                    clientApp = new WhiteBoxApplication(transport);
+                    return clientApp;
                 });
     }
 
@@ -51,18 +48,41 @@ class SamplesForAsyncTcpTest
     void establishConnection()
     {
         final int serverPort = freePort();
-        serverTransport.handle(serverTransport.command(Listen.class).set(1, serverPort));
-        assertThat(serverEvents.lastResponse(StartedListening.class, 1).port()).isEqualTo(serverPort);
+        serverApp.transport.handle(serverApp.transport.command(Listen.class).set(1, serverPort));
+        assertThat(serverApp.events.lastResponse(StartedListening.class, 1).port()).isEqualTo(serverPort);
 
         // When
-        clientTransport.handle(clientTransport.command(Connect.class).set("localhost", serverPort, 2, 100));
+        clientApp.transport.handle(clientApp.transport.command(Connect.class).set("localhost", serverPort, 2, 100));
 
         // Then
-        Connected connected = clientEvents.lastResponse(Connected.class, 2);
+        Connected connected = clientApp.events.lastResponse(Connected.class, 2);
         assertThat(connected.port()).isGreaterThan(0);
         assertThat(connected.remotePort()).isEqualTo(serverPort);
         assertThat(connected.connectionId()).isGreaterThan(0);
         assertThat(connected.inboundPduLimit()).isGreaterThan(0);
         assertThat(connected.outboundPduLimit()).isGreaterThan(0);
+    }
+
+    /**
+     * A standard app should by autonomous and reactive.
+     * It should not require external control apart from the injection of dependencies via a constructor.
+     * This is not a standard app. All the logic lives outside it to show the transport API in the tests.
+     */
+    static class WhiteBoxApplication implements Application
+    {
+        private final TransportEventsSpy events = new TransportEventsSpy();
+        private final TransportEventsRedirect eventsRedirect = new TransportEventsRedirect(events);
+        private final Transport transport;
+
+        WhiteBoxApplication(final Transport transport)
+        {
+            this.transport = transport;
+        }
+
+        @Override
+        public void onEvent(final Event event)
+        {
+            eventsRedirect.onEvent(event);
+        }
     }
 }
