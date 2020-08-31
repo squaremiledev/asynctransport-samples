44daf76aaa6aeda595be7c3fda4d1562243c9b77
Michael Szymczak <contact@michaelszymczak.com>
2020-08-31 14:53:29 +0100

    use helper classes from the library fixtures instead of rolling your own

--- asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
+++ asynctcpsamples/src/test/java/dev/squaremile/asynctransportsamples/asynctcpsamples/SamplesForAsyncTcpTest.java
@@ -1,45 +1,40 @@
 package dev.squaremile.asynctransportsamples.asynctcpsamples;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-import dev.squaremile.asynctcp.application.Application;
 import dev.squaremile.asynctcp.application.TransportAppLauncher;
-import dev.squaremile.asynctcp.domain.api.Transport;
 import dev.squaremile.asynctcp.domain.api.commands.Connect;
 import dev.squaremile.asynctcp.domain.api.commands.Listen;
 import dev.squaremile.asynctcp.domain.api.events.Connected;
-import dev.squaremile.asynctcp.domain.api.events.Event;
 import dev.squaremile.asynctcp.domain.api.events.StartedListening;
-import dev.squaremile.asynctcp.testfitures.TransportEventsSpy;
+import dev.squaremile.asynctcp.testfitures.app.WhiteboxApplication;
 
 import static dev.squaremile.asynctcp.testfitures.FreePort.freePort;
 
 class SamplesForAsyncTcpTest
 {
-    private WhiteBoxApplication serverApp;
-    private WhiteBoxApplication clientApp;
+    private WhiteboxApplication serverApp;
+    private WhiteboxApplication clientApp;
 
     @BeforeEach
-    void setUp() throws IOException
+    void setUp()
     {
         new TransportAppLauncher().launch(
                 transport ->
                 {
                     // Store the reference to manipulate the underlying transport directly from the test.
                     // A standard app does not require such things.
-                    serverApp = new WhiteBoxApplication(transport);
+                    serverApp = new WhiteboxApplication(transport);
                     return serverApp;
                 });
         new TransportAppLauncher().launch(
                 transport ->
                 {
-                    clientApp = new WhiteBoxApplication(transport);
+                    clientApp = new WhiteboxApplication(transport);
                     return clientApp;
                 });
     }
@@ -48,41 +43,18 @@ class SamplesForAsyncTcpTest
     void establishConnection()
     {
         final int serverPort = freePort();
-        serverApp.transport.handle(serverApp.transport.command(Listen.class).set(1, serverPort));
-        assertThat(serverApp.events.lastResponse(StartedListening.class, 1).port()).isEqualTo(serverPort);
+        serverApp.underlyingtTansport().handle(serverApp.underlyingtTansport().command(Listen.class).set(1, serverPort));
+        assertThat(serverApp.events().lastResponse(StartedListening.class, 1).port()).isEqualTo(serverPort);
 
         // When
-        clientApp.transport.handle(clientApp.transport.command(Connect.class).set("localhost", serverPort, 2, 100));
+        clientApp.underlyingtTansport().handle(clientApp.underlyingtTansport().command(Connect.class).set("localhost", serverPort, 2, 100));
 
         // Then
-        Connected connected = clientApp.events.lastResponse(Connected.class, 2);
+        Connected connected = clientApp.events().lastResponse(Connected.class, 2);
         assertThat(connected.port()).isGreaterThan(0);
         assertThat(connected.remotePort()).isEqualTo(serverPort);
         assertThat(connected.connectionId()).isGreaterThan(0);
         assertThat(connected.inboundPduLimit()).isGreaterThan(0);
         assertThat(connected.outboundPduLimit()).isGreaterThan(0);
-    }
-
-    /**
-     * A standard app should by autonomous and reactive.
-     * It should not require external control apart from the injection of dependencies via a constructor.
-     * This is not a standard app. All the logic lives outside it to show the transport API in the tests.
-     */
-    static class WhiteBoxApplication implements Application
-    {
-        private final TransportEventsSpy events = new TransportEventsSpy();
-        private final TransportEventsRedirect eventsRedirect = new TransportEventsRedirect(events);
-        private final Transport transport;
-
-        WhiteBoxApplication(final Transport transport)
-        {
-            this.transport = transport;
-        }
-
-        @Override
-        public void onEvent(final Event event)
-        {
-            eventsRedirect.onEvent(event);
-        }
     }
 }
