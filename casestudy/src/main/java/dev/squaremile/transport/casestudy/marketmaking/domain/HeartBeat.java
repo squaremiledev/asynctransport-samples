cfbc5adc7a3e5b59d76da7334321da62cbb15939
Michael Szymczak <contact@michaelszymczak.com>
2021-01-08 20:08:26 +0000

    Move the market making simulation from the async-tcp
    This simulation allows spotting the places that fail to follow the mid price due to e.g. GC pauses
    
    To render the chart:
    
    1. Create /tmp/asynctransportmarketmaking directory, download dygraph.js and dygraph.css from https://dygraphs.com/ and copy the files there
    2. Run MarketMakingTest::runSimulation
    3. Go to /tmp/asynctransportmarketmaking and start some web server, such as http-server (sudo npm install http-server -g)
    4. Open the page and wait a few seconds (the graph library needs to process the data)
    5. The result should be similar to https://github.com/squaremiledev/asynctransport/issues/22#issuecomment-750361201

---
+++ casestudy/src/main/java/dev/squaremile/transport/casestudy/marketmaking/domain/HeartBeat.java
@@ -0,0 +1,6 @@
+package dev.squaremile.transport.casestudy.marketmaking.domain;
+
+public class HeartBeat implements MarketMessage
+{
+    public static final HeartBeat INSTANCE = new HeartBeat();
+}
